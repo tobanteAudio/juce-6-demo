b99066da2f999fd1caa33e9d751b2a9d7d9084ec
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-04-16 03:02:56 +0200

    Cleanup processor

--- plugin/plugin_processor.cpp
+++ plugin/plugin_processor.cpp
@@ -3,12 +3,8 @@
 
 Juce6DemoProcessor::Juce6DemoProcessor()
     : AudioProcessor(BusesProperties()
-#if !JucePlugin_IsMidiEffect
-#if !JucePlugin_IsSynth
                          .withInput("Input", juce::AudioChannelSet::stereo(), true)
-#endif
                          .withOutput("Output", juce::AudioChannelSet::stereo(), true)
-#endif
                          )
     , undoManager_ {}
     , parameters_ {
@@ -39,38 +35,27 @@ const juce::String Juce6DemoProcessor::getName() const { return JucePlugin_Name;
 
 bool Juce6DemoProcessor::acceptsMidi() const
 {
-#if JucePlugin_WantsMidiInput
-    return true;
-#else
     return false;
-#endif
 }
 
 bool Juce6DemoProcessor::producesMidi() const
 {
-#if JucePlugin_ProducesMidiOutput
-    return true;
-#else
     return false;
-#endif
 }
 
 bool Juce6DemoProcessor::isMidiEffect() const
 {
-#if JucePlugin_IsMidiEffect
-    return true;
-#else
     return false;
-#endif
 }
 
 double Juce6DemoProcessor::getTailLengthSeconds() const { return 0.0; }
 
 int Juce6DemoProcessor::getNumPrograms()
 {
-    return 1;  // NB: some hosts don't cope very well if you tell them there are
-               // 0 programs, so this should be at least 1, even if you're not
-               // really implementing programs.
+    // NB: some hosts don't cope very well if you tell them there are
+    // 0 programs, so this should be at least 1, even if you're not
+    // really implementing programs.
+    return 1;
 }
 
 int Juce6DemoProcessor::getCurrentProgram() { return 0; }
@@ -114,23 +99,14 @@ void Juce6DemoProcessor::releaseResources() { }
 
 bool Juce6DemoProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
 {
-#if JucePlugin_IsMidiEffect
-    juce::ignoreUnused(layouts);
-    return true;
-#else
     // This is the place where you check if the layout is supported.
     // In this template code we only support mono or stereo.
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
         && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
+    { return false; }
 
-        // This checks if the input layout matches the output layout
-#if !JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet()) return false;
-#endif
-
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet()) { return false; }
     return true;
-#endif
 }
 
 void Juce6DemoProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
